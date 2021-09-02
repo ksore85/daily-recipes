a4fa0b2bda6f1e6f25ec944d18f7576423b0b1f5
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-09-01 20:28:06 -0500

    build passport.js google login

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,10 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const authRoutes = require('./auth-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
-
+router.use('/auth', authRoutes);
 module.exports = router;
