ad536f0fcbfaf63b24de883c2989c11f6b9d6d47
maris <marisandb@yahoo.com>
2021-09-01 20:40:26 -0500

    added carousel and made some other adjustments

--- public/javascript/css.js
+++ public/javascript/css.js
@@ -1,19 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //find wich slide is the middle one
-    // var carousel_items = document.querySelectorAll('.carousel-item').length;
-    // var middle_slide = Math.round(carousel_items / 2) 
-    
-    // console.log('The slide in the middle is the ' + middle_slide + ' out of ' + carousel_items)
-    //Carousel initialization
     var options = {};
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, options);
-    
-    //Set the carousel to show the middle slide first
-    // var elem = document.querySelector('.carousel');
-    // var instance = M.Carousel.getInstance(elem);
-    // instance.set(middle_slide);
+     
     var instance = M.Carousel.init({
-        fullWidth: true
-      });
+        fullWidth: true,
+        duration: 75
+    });
 })
