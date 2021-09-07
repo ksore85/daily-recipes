7dbf92627067b87d8d63bb1bc6a64b3c1aad670f
maris <marisandb@yahoo.com>
2021-09-06 21:54:24 -0500

    fixed broken randome recipe generator, fixed broken button feature

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -91,51 +91,40 @@ router.get('/post/:id', (req, res) => {
 });
 
 // // get random recipe
-router.get('/recipe-of-the-day', (req, res) => {
-  Post.findOne({
-    order: sequelize.random('id'),
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
+router.get('/random-recipe', (req, res) => {
+ Post.findAll({
+   order: sequelize.random(),
+   limit: 1,
+   attributes: [
+    'id',
+    'post_url',
+    'title',
+    'created_at',
+    [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+  ],
+   include: [
+    {
+      model: Comment,
+      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+      include: {
         model: User,
         attributes: ['username']
       }
-    ]
-  })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
+    },
+    {
+      model: User,
+      attributes: ['username']
+    }
+  ]
+ }).then(randomPost => {
+  const post = randomPost[0].get({ plain: true })
 
-      const post = dbPostData.get({ plain: true });
-
-      res.render('random-post', {
-        post,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  res.render('random-post', {post: post,
+  loggedIn: req.session.loggedIn
+  }) 
+   
+  
+ })
 });
 
 
